d1a859baf8fdc6052d453f3f0d77fecfcd3e06a4
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-03-17 15:17:04 -0400

    init

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { API_URL } from '../app.constants';
 import { Product } from '../types/product.interface';
+import { first } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
 
-  products$ = this.http.get<Product[]>(`${API_URL}/products`);
+  products$ = this.http.get<Product[]>(`${API_URL}/products`).pipe(first());
 
   find(id: string) {
     return this.http.get<Product>(`${API_URL}/products/${id}`);
